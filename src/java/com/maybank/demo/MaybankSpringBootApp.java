381f9abc913a2f49c7953970bcf9113e8148445c
rainyPoseidon21 <muhaimiemazlah0116346@gmail.com>
2024-09-24 22:35:42 +0800

    Adding gradle config, Adding base spring boot code

--- src/java/com/maybank/demo/MaybankSpringBootApp.java
+++
@@ -1,7 +0,0 @@
-package java.com.maybank.demo;
-
-public class MaybankSpringBootApp {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/maybank/demo/MaybankDemoApplication.java
@@ -0,0 +1,13 @@
+package com.maybank.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaybankDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaybankDemoApplication.class, args);
+	}
+
+}
